bfc244dde7a85d208781ffa23ab62a0e3b0a098a
Diego Lago <diego.lago.gonzalez@gmail.com>
2014-10-14 10:18:49 +0200

    Refactor private variables and implement function to print the result.

--- gray-codes/Gray.java
+++ gray-codes/Gray.java
@@ -1,19 +1,40 @@
 
 class Gray {
 
+	private static int _dym = 0;
+	private static int _size = 0;
 	private static boolean[][] result;
 
-	private static void precondition(int dym) throws Exception {
-		if (dym <= 0) {
-			throw new Exception("Invalid dymension.");
+	private static void precondition() throws Exception {
+		if (_dym <= 0) {
+			throw new Exception("Invalid _dymension.");
 		}
 	}
 
-	private static void reserveSize(int dym) {
-		int size = dym ^ 2;
-		result = new boolean[size][];
-		for (int i = 0; i < size; i++) {
-			result[i] = new boolean[dym];
+	public static void d(String msg) {
+		System.out.println(msg);
+	}
+
+	private static void reserveSize() {
+		result = new boolean[_size][];
+		for (int i = 0; i < _size; i++) {
+			result[i] = new boolean[_dym];
+			for (int j = 0; j < _dym; j++) {
+				result[i][j] = false;
+			}
+		}
+	}
+
+	private static void printResult(boolean vertical) {
+		for (int i = 0; i < _size; i++) {
+			for (int j = 0; j < _dym; j++) {
+				System.out.print(result[i][j] ? "1" : "0");
+			}
+			if (vertical) {
+				System.out.println();
+			} else {
+				System.out.print(" ");
+			}
 		}
 	}
 
@@ -22,7 +43,7 @@ class Gray {
 		System.out.println("Usage: gray <dimension>");
 	}
 
-	private static void calculateGrayCodes(int dimension) {
+	private static void calculateGrayCodes() {
 
 	}
 
@@ -31,10 +52,12 @@ class Gray {
 			printHelp();
 		} else {
 			try {
-				int dym = Integer.parseInt(args[0]);
-				precondition(dym);
-				reserveSize(dym);
-				calculateGrayCodes(dym);
+				_dym = Integer.parseInt(args[0]);
+				_size = (int)Math.pow(_dym, 2);
+				precondition();
+				reserveSize();
+				calculateGrayCodes();
+				printResult(true);
 			} catch(Exception e) {
 				System.out.println("ERROR: " + e.getMessage());
 			}
