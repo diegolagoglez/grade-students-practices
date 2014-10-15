1be01226309fc1e76f5d5b70a6a3ddb09e8927c0
Diego Lago <diego.lago.gonzalez@gmail.com>
2014-10-15 08:26:33 +0200

    More about Gray codes.

--- gray-codes/Gray.java
+++ gray-codes/Gray.java
@@ -3,11 +3,11 @@ class Gray {
 
 	private static int _dym = 0;
 	private static int _size = 0;
-	private static boolean[][] result;
+	private static boolean[][] _result;
 
 	private static void precondition() throws Exception {
-		if (_dym <= 0) {
-			throw new Exception("Invalid _dymension.");
+		if (_dym <= 0 || _dym > 32) {
+			throw new Exception("Invalid dymension.");
 		}
 	}
 
@@ -15,20 +15,22 @@ class Gray {
 		System.out.println(msg);
 	}
 
-	private static void reserveSize() {
-		result = new boolean[_size][];
-		for (int i = 0; i < _size; i++) {
-			result[i] = new boolean[_dym];
-			for (int j = 0; j < _dym; j++) {
+	private static boolean[][] reserveSize(int dym) {
+		int size = (int)Math.pow(dym, 2);
+		boolean[][] result = new boolean[size][];
+		for (int i = 0; i < size; i++) {
+			result[i] = new boolean[dym];
+			for (int j = 0; j < dym; j++) {
 				result[i][j] = false;
 			}
 		}
+		return result;
 	}
 
 	private static void printResult(boolean vertical) {
 		for (int i = 0; i < _size; i++) {
 			for (int j = 0; j < _dym; j++) {
-				System.out.print(result[i][j] ? "1" : "0");
+				System.out.print(_result[i][j] ? "1" : "0");
 			}
 			if (vertical) {
 				System.out.println();
@@ -43,8 +45,15 @@ class Gray {
 		System.out.println("Usage: gray <dimension>");
 	}
 
-	private static void calculateGrayCodes() {
-
+	private static boolean[][] calculateGrayCodes(int dym, boolean[][] m) {
+		if (dym == 1) {
+			boolean[][] a = reserveSize(1);
+			boolean[0][0] = 0;
+			boolean[0][1] = 1;
+			return a;
+		} else {
+			return calculateGrayCodes(dym - 1, m);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -55,8 +64,8 @@ class Gray {
 				_dym = Integer.parseInt(args[0]);
 				_size = (int)Math.pow(_dym, 2);
 				precondition();
-				reserveSize();
-				calculateGrayCodes();
+				_result = reserveSize(_dym);
+				calculateGrayCodes(_dym, _result);
 				printResult(true);
 			} catch(Exception e) {
 				System.out.println("ERROR: " + e.getMessage());
